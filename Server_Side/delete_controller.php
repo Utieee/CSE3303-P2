1beb25220ec907fdc9a156f96cd83cff45341b8e
hxn8439 <52980606+hxn8439@users.noreply.github.com>
2020-05-03 19:35:48 -0500

    update insert and delete controller
    updated two serverside php files.

--- Server_Side/delete_controller.php
+++ Server_Side/delete_controller.php
@@ -3,60 +3,78 @@
 $host = "localhost";
 $dbusername = "root";
 $dbpassword = "";
-$dbname="doctoral";
+$dbname="renim";
 
 //Create Connection
-
-$conn = new PDO ('mysql:host='.$host.'; dbname='.$dbname, $dbusername, $dbpassword);
+$conn = mysqli_connect($host, $dbusername, $dbpassword, $dbname);
 
 //check Connection
 if(!$conn)
 {
-	die("Connection failed");
+	die("Connection failed" . mysqli_connect_error());
 }	
 
 //start processing
 
 else
 {
-	$InstructorId = filter_input(INPUT_POST,'InstructorId');
-	$FName = filter_input(INPUT_POST,'FName');
-	$LName = filter_input(INPUT_POST,'LName');
-	$StartDate = filter_input(INPUT_POST,'StartDate');
-	$Degree = filter_input(INPUT_POST,'Degree');
-	$Rank = filter_input(INPUT_POST,'Rank');
-	$Type = filter_input(INPUT_POST,'Type');
+	$spid = filter_input(INPUT_POST,'Pid');
 	
-	$CourseId = filter_input(INPUT_POST, 'CourseId');
-	$StudentId = filter_input(INPUT_POST, 'StudentId');
+	if (isset($_POST["submit"]))
+	{	
 
-	$query = "INSERT INTO instructor(InstructorId, FName, LName, StartDate, Degree, Rank, Type)
-	values('$InstructorId','$FName','$LName','$StartDate','$Degree','$Rank','$Type')";
+		if(empty($spid))
+		{
+			echo "<font color='red'>***YOU MUST FILL THE DATA FIELD.***</font><br/><br/>";
+		}	
+		
+		else
+		{	
+			$query = "SELECT * FROM end_product WHERE ProductID = '". mysqli_real_escape_string($conn,$spid) ."'";
+			$result = mysqli_query($conn,$query);
+			
+			if (mysqli_num_rows($result) == 1) 
+			{
+				$row = mysqli_fetch_assoc($result);
+				$cpid = $row["Pid"];
+				$query1 = "SELECT * FROM buyer_endproduct WHERE BEProductID = '". mysqli_real_escape_string($conn,$cpid) ."'";
+				$result1 = mysqli_query($conn,$query1);
+				
+				if (mysqli_num_rows($result1) == 1) 
+				{
+					echo "<font color='red'>***MATCHED FOR ProductID IN Buyer_EndProduct TABLE.***</font><br/><br/>";
+					echo "<font color='red'>***THE DATA FROM SELECTED ProductID is retained..***</font><br/><br/>";
+					die();
+				}
+				
+				else 
+				{
+					echo "<font color='red'>***THE ProductID DOES NOT EXIST IN THE Buyer_EndProduct TABLE.***</font><br/><br/>";
+					
+					$query2 = "DELETE FROM end_product WHERE end_product.ProductID = '$cpid'";
+				 	
+					if (mysqli_query($conn, $query2))
+					{
+						echo "Deleted successfully in end_product table, the data is no longer in the database.</font><br/><br/>";
+					}
 
-	$query1 = "INSERT INTO coursestaught(CourseId, InstructorId)values('$CourseId','$InstructorId')";
-	
-	$query2 = "INSERT INTO phdcommittee(StudentId, InstructorId)values('$StudentId','$InstructorId')";
-
-	if($conn->query($query))
-	{
-		echo "NEW RECORD ADDED SUCCESSFULLY IN INSTRUCTOR TABLE"; echo'</br>';
-	}
-	
-	if($conn->query($query1))
-	{
-		echo "NEW RECORD ADDED SUCCESSFULLY IN CourseTaught TABLE"; echo'</br>';
-	}
-	
-	if($conn->query($query2))
-	{
-		echo "NEW RECORD ADDED SUCCESSFULLY IN phdcommittee TABLE"; echo'</br>';
+					
+					else 
+					{
+						echo "<font color='red'>***ERROR IN DELETING DATA FROM END_PRODUCT TABLE***</font><br/><br/>";
+						die();
+					}	
+				}
+			} 
+			
+			else 
+			{
+				echo "<font color='red'>***NO MATCH FOR ProductID IN END_PRODUCT TABLE.***</font><br/><br/>";
+				die();
+			}
+		}
 	}	
-
-	else
-	{
-		echo "Error:".$query."".$conn->error;
-	}		
-	
 }	
 
+mysqli_close($conn);
 ?>
